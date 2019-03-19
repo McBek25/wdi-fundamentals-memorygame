e8cf1d3df4b4a030f03a48ba3669994e7dcb86ed
Rebekah McDevitt <bek@rebekahs-mbp.attlocal.net>
2019-03-19 16:05:09 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,36 @@
 console.log("Up and running!");
 
-var cardOne= "queen";
-var cardTwo= "queen";
-var cardThree= "king";
-var cardFour= "king";
+
+
+
+
+var cards = ["queen", "queen", "king", "king"]
+
+cardsInPlay = []
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+cardsInPlay;
+
 
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+cardsInPlay;
+
+console.log("User flipped " + cardTwo);
+
+
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
