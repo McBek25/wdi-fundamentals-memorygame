e224e86032d56f60c4ddd4e9c977514cf804ce2f
Rebekah McDevitt <bek@rebekahs-mbp.attlocal.net>
2019-03-20 11:18:14 -0400

    created game logic (still buggy)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -36,7 +36,7 @@ var cardsInPlay = [];
 
 
 
-function checkForMatch () {
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
 	} else {
@@ -44,22 +44,39 @@ function checkForMatch () {
 	}
 };
 
-checkForMatch();
+
+
+
+//Unable to determine why var cardID is undefined when following pre-work instructions.  Will continue to go back and debug before Monday.
 
 var flipCard = function () {
-	
-
-
-//Unable to determine why var cardID is undefined when following pre-work instructions.
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length === 2){
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	checkForMatch();
+	if (cardsInPlay.length === 2) {
 		checkForMatch();
-	} 
-};
+	};
+	
+}; 
+
 
 flipCard();
 
-flipCard(0);
-flipCard(2);
+
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementbyId('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
 
