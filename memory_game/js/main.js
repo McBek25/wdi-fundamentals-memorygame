8fad4bde15240462b8238f73e2425db05d66ced8
Rebekah McDevitt <bek@rebekahs-mbp.attlocal.net>
2019-03-20 10:40:36 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,9 +4,31 @@ console.log("Up and running!");
 
 
 
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
 
-var cardsInPlay = []
+
+var cardsInPlay = [];
 
 
 
@@ -24,15 +46,20 @@ function checkForMatch () {
 
 checkForMatch();
 
-function flipCard () {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
+var flipCard = function () {
 	
+
+
+//Unable to determine why var cardID is undefined when following pre-work instructions.
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2){
+		checkForMatch();
 	} 
 };
 
-
 flipCard();
+
 flipCard(0);
 flipCard(2);
+
