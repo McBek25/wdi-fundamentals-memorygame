21cc0b9f91d6f3a80f6d72e50b93a421023f440a
Rebekah McDevitt <bek@rebekahs-mbp.attlocal.net>
2019-03-31 23:34:25 -0400

    created game logic, third commit, project redo

--- memory_game/js/main2.js
+++ memory_game/js/main2.js
@@ -7,22 +7,44 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
+/* comment out. code to be deleted per instructions:
+
 var cardOne = cards[0];
-
 cardsInPlay.push(cardOne);
-
 console.log("User flipped " + cardOne);
 
 var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
-
 console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
+*/
+
+function checkForMatch () {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("Sorry, try again.");
-    }
+    } //example in slides had console.log
     
 };
+
+function flipCard (cardId) {
+    
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) {
+        checkForMatch ();
+   
+        
+        
+    };
+
+};
+
+flipCard (2);
+
+
+
+
+
+
